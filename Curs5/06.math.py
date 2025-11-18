eb170a15658a4a1d7f07f36e16c77c9ae4e8e7e0
Silviu Ojog <sil.ojog@gmail.com>
2025-11-18 20:16:28 +0200

    Exercitiu bancnote

---
+++ Curs5/06.math.py
@@ -0,0 +1,15 @@
+
+## modul (fisier) - importat din locatia in care este instalat python sau din propriul folder
+import math
+
+print(math.pi)
+
+print(math.sqrt(16))
+
+print(math.ceil(16.31))
+
+print(math.floor(16.91))
+
+import modul_meu
+
+print(modul_meu.variabila_din_modulul_meu)
